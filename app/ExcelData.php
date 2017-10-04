f72cdb4ab5260f346c4a7708a795b014c8edc1a5
sergey_h <hashimov.sergey@.gmail.com>
2017-10-04 10:05:19 +0300

    Добавить поле "Количество" для обновления в бд информации об остатках

--- app/ExcelData.php
+++ app/ExcelData.php
@@ -11,13 +11,15 @@ class ExcelData
     public $price;
     public $manufacturer;
     public $currency_id = null;
+    public $stock;
 
-    public function __construct($sku, $price, $manufacturer, $currency)
+    public function __construct($sku, $price, $manufacturer, $currency, $stock)
     {
         $this->sku = $sku;
         $this->price = $price;
         $this->currency_id = $currency;
         $this->manufacturer = $manufacturer;
+        $this->stock = $stock;
     }
 
 
